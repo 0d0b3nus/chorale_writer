b9f6d89826ad6803c3aaf0ff1de55da55be514e4
Darko Trifunovski <dtrifuno@gmail.com>
2016-06-25 09:48:01 -0500

    Linting

--- util.py
+++ util.py
@@ -7,14 +7,14 @@ ROMAN_NUMERAL_DICT = OrderedDict([
     (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
     ])
 
-def to_roman(n):
+def to_roman(num):
     """ Given int between 1 and 4000 returns corresponding Roman numeral. """
-    if not 1 <= n <= 4000:
+    if not 1 <= num <= 4000:
         raise ValueError('to_roman can only convert integers between 1 and '
                          '3999')
     result = ""
     for roman_value in ROMAN_NUMERAL_DICT.keys():
-        how_many, remainder = divmod(n, roman_value)
+        how_many, remainder = divmod(num, roman_value)
         result += ROMAN_NUMERAL_DICT[roman_value] * how_many
-        n = remainder
+        num = remainder
     return result
