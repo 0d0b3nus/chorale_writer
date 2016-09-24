a2c0784ba1296854f861836c96fd6abfa6d6b45a
Darko Trifunovski <dtrifuno@gmail.com>
2016-09-24 11:52:37 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='regis',
+      version='0.1',
+      description='',
+      url='https://github.com/dtrifuno/regis',
+      author='Darko Trifunovski',
+      author_email='dtrifuno@gmail.com',
+      license='GPL3',
+      packages=['markovify', 'PyQt5'],
+      zip_safe=False)
