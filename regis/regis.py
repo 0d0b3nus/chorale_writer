78c33501742b0619ee59c20d5388022b56ac4355
Darko Trifunovski <dtrifuno@gmail.com>
2016-10-17 20:40:45 -0500

    More progress on GUI

--- regis/regis.py
+++ regis/regis.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton, QHBoxLayout, QVBoxLayout,
-        QApplication)
+        QLabel, QComboBox, QGridLayout, QApplication)
+import PyQt5.QtCore
 
 class StartWindow(QWidget):
 
@@ -9,30 +10,65 @@ class StartWindow(QWidget):
         self.initUI()
 
     def initUI(self):
+        regisLabel = QLabel("<b>Regis</b>")
+        regisLabel.setAlignment(PyQt5.QtCore.Qt.AlignCenter)
+        procLabel = QLabel("Procedural Chorale Generator")
+        procLabel.setAlignment(PyQt5.QtCore.Qt.AlignCenter)
+
         newButton = QPushButton("New Chorale")
         exerciseButton = QPushButton("Exercise Mode")
         quitButton = QPushButton ("Quit")
 
+        exerciseButton.clicked.connect(self.close)
         quitButton.clicked.connect(self.close)
 
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addWidget(newButton)
-        vbox.addWidget(exerciseButton)
-        vbox.addWidget(quitButton)
+        grid = QGridLayout()
 
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addLayout(vbox)
+        grid.addWidget(regisLabel, 0, 1)
+        grid.addWidget(procLabel, 1, 1)
+        grid.addWidget(newButton, 2, 1)
+        grid.addWidget(exerciseButton, 3, 1)
+        grid.addWidget(quitButton, 4, 1)
 
-        self.setLayout(hbox)
+        self.setLayout(grid)
 
-        self.setGeometry(300, 300, 300, 150)
         self.setWindowTitle('Regis')
+        self.show()
+
+class ExerciseWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        size = 5
+        grid = QGridLayout()
+
+        labels = [''] * size
+        dropdowns = [''] * size
+        checks = [''] * size
+        for i in range(size):
+            labels[i] = QLabel("{}".format(i+1))
+            grid.addWidget(labels[i], i+1, 0)
+            dropdowns[i] = QComboBox(self)
+            grid.addWidget(dropdowns[i], i+1, 2)
+
+        gradeButton = QPushButton('Grade')
+        grid.addWidget(gradeButton, i+size+1, 0)
+        newButton = QPushButton('New')
+        grid.addWidget(newButton, i+size+1, 2)
+        backButton = QPushButton('Back')
+        grid.addWidget(backButton, i+size+1, 4)
+
+        self.setLayout(grid)
+
+        self.setWindowTitle('Regis: Exercise Mode')
         self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = StartWindow()
+    exs = ExerciseWindow()
     sys.exit(app.exec())
